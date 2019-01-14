cba965606c5233e943063bea296f1cd6cc1f6d0f
Kevin Ossa <kevin.ossa@myquestis.com>
2019-01-14 09:41:31 -0500

    Added Flask bootstrap, re-did frontend

--- simple_media/app/__init__.py
+++ simple_media/app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
+Bootstrap = Bootstrap(app)
 login.login_view = 'login'
 
 if not os.path.exists('logs'):
